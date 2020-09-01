2e3e9ed7947a576320ca6cd927f94baee1d0572c
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-31 22:22:56 -0300

    Adicionado formulário

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.buscarDados = this.buscarDados.bind(this);
+  }
+
+  buscarDados(){
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <form action={this.buscarDados}>
+          <input id="nome" type="text" name="nome"/>
+          <input type="submit" value="Enviar"/>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
